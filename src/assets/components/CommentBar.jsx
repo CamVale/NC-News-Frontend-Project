000116af2dc66ccd89839778b229bcc11b3dae2d
CamVale <cameronvale1@gmail.com>
2023-12-13 14:43:50 +0000

    implements feedback

--- src/assets/components/CommentBar.jsx
+++ src/assets/components/CommentBar.jsx
@@ -1,19 +1,29 @@
 import { useEffect, useState } from "react";
 import { getComments } from "../api/api";
-import { Badge, ListGroup } from "react-bootstrap";
+import { Badge, ListGroup, Spinner } from "react-bootstrap";
 
 export default function CommentBar(props) {
   const [currComments, setCurrComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
 
-  console.log(props.currID, "commentbar");
 
   useEffect(() => {
     getComments(props.currID).then((res) => {
       setCurrComments(res);
+      setIsLoading(false)
     });
   }, []);
 
-  return (
+  if (isLoading) {
+    return (<>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner><h1>Loading...</h1>
+      </>
+    );
+  }
+
+  return currComments.length > 0 ? (
     <ListGroup>
       {currComments.map((comment) => {
         return (
@@ -29,5 +39,13 @@ export default function CommentBar(props) {
         );
       })}
     </ListGroup>
-  );
+  ) : (
+    <ListGroup>
+      <ListGroup.Item>
+      <div className="ms-2 me-auto">
+        <div className="fw-bold">No comments yet...</div>
+      </div>
+      </ListGroup.Item>
+    </ListGroup>
+  )
 }
