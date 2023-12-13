0198021c9e8bca9d44ae666a7a8e0deb22fe8888
CamVale <cameronvale1@gmail.com>
2023-12-13 11:39:53 +0000

    adds loading element

--- src/assets/pages/Articles.jsx
+++ src/assets/pages/Articles.jsx
@@ -1,28 +1,38 @@
-import { useState } from "react"
-import ArticleCard from "../components/ArticleCard"
+import { useState } from "react";
+import ArticleCard from "../components/ArticleCard";
 import { getArticles } from "../api/api";
 import { useEffect } from "react";
 import { MDBTypography } from "mdb-react-ui-kit";
+import { Spinner } from "react-bootstrap";
 
+export default function Articles() {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default function Articles(){
-
-    const [articles, setArticles] = useState([])
-
-    useEffect(() => {
-        getArticles().then((res) => {
+  
+  useEffect(() => {
+      getArticles().then((res) => {
           setArticles(res);
+          setIsLoading(false)
         });
-      }, []);
-
-    return (
-        <>
-        <h1>Articles!</h1>
-        <MDBTypography listUnStyled className='mb-0'>
-        {articles.map((article, index)=>{
-          return <ArticleCard article={article} key={index}/>
+    }, []);
+    
+    if (isLoading) {
+      return (<>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner><h1>Loading...</h1>
+        </>
+      );
+    }
+  return (
+    <>
+      <h1>Articles!</h1>
+      <MDBTypography listUnStyled className="mb-0">
+        {articles.map((article, index) => {
+          return <ArticleCard article={article} key={index} />;
         })}
       </MDBTypography>
-      </>
-    )
+    </>
+  );
 }
